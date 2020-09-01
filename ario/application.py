5c3296dd795e55de68b43dcf826a500ab3e19d24
nothingrealm <shafaghi1378@gmail.com>
2020-09-01 14:18:37 +0430

    Bug fixed in Application init

--- ario/application.py
+++ ario/application.py
@@ -3,8 +3,9 @@
 class Application:
     def __init__(self, controller=None):
         self.routes = []
-        if controller in not None:
-            self.controller= None
+        if controller is not None:
+            self.controller= controller
 
     def __call__(self, environ, start_response):
-        response = self.controller(environ)
+        response = self.controller(environ, start_response)
+        return response
