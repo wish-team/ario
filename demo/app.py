3b1f9f4e186ca541574cc79960bf34a7ff5ec3c9
nothingrealm <shafaghi1378@gmail.com>
2020-09-02 17:21:16 +0430

    Testing the request

--- demo/app.py
+++ demo/app.py
@@ -5,11 +5,19 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from ario import Application
+from ario import Application, RouterController, Endpoint, Request 
 
+control = RouterController()
 
 def controller(environ, start_response):
     """Simplest possible application object"""
+    req = Request(environ)
+    length = req.content_length
+    print(length)
+    met = req.method
+    print(met)
+    path = req.path
+    print(path)
     data = b'Hello, World!\n'
     status = '200 OK'
     response_headers = [
@@ -20,4 +28,4 @@ def controller(environ, start_response):
     return iter([data])
 
 
-app = Application(controller)
+app = Application(control)
