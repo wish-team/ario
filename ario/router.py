14f75f77c68312ce33313041ba8ab6dd65ba3a62
nothingrealm <shafaghi1378@gmail.com>
2020-09-03 11:00:22 +0430

    New mechanism for routing

--- ario/router.py
+++ ario/router.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+from ario.request import Request
 
 
 @dataclass
@@ -33,23 +34,44 @@ class RouterController:
 
     def __init__(self):
         if RouterController.__instance is None:
-            self.routes = []
+            self.routes = {
+                    "path": "/",
+                    "method": [],
+                    "handler": None,
+                    "childs": []
+                }
             RouterController.__instance = self
         else:
             raise Exception("Controller already instantiated")
 
 
-    def __call__(self):
-        pass
-    
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+        method  = req.method
+        path = req.path
+
 
     def route(self, method, route):
-        print(method)
         def wrapper(cls):
-            print(cls)
-            router = cls(method, route)
-            self.routes.append(router)
-
+            print(route)
+            route = route.replace("/", "", 1)
+            tokens = route.split("/")
+            routes = self.routes
+            for i in range(len(tokens)):
+                if routes["path"] != tokens[i]:
+                    continue 
+                if len(tokens) - 1 == i:
+                    new_route = {
+                        "path": tokens[i],
+                        "method": [method],
+                        "handler": cls(method, route),
+                        "childs": []
+                    }
+                    routes['childs'].append(new_route)
+                    break
+                else:
+                    routes = routes["childs"]
+            print(routes)
         return wrapper
 
 
