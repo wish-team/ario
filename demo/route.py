3839da8183b3f61b17d7604a0c64c154f2a06f90
Shayan <shafaghi1378@gmail.com>
2020-09-04 20:05:15 +0430

    Made application to test with gunicorn

--- demo/route.py
+++ demo/route.py
@@ -5,20 +5,22 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-from ario import RouterController, Endpoint 
+from ario import RouterController, Endpoint, Application
 
 control = RouterController()
 
-@control.route(method=["GET"], route="/user/test/foo/baz/bar")
+@control.route(method=["GET", "PUT"], route="/user")
 class UserEndpoint(Endpoint):
     pass
 
 
-@control.route(method=["GET"], route="/dashboard/baz/cux")
+@control.route(method=["GET", "POST"], route="/dashboard/baz")
 class DashboardEndpoint(Endpoint):
     pass
 
 
-@control.route(method=["GET"], route="/user/test/foo")
+@control.route(method=["GET", "POST"], route="/user/test")
 class DashboardEndpoint(Endpoint):
     pass
+
+app = Application(control)
