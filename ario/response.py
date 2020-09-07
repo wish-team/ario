bdef1010e3308bc7601268d8ca4143684362b67e
nothingrealm <shafaghi1378@gmail.com>
2020-09-07 19:40:56 +0430

    added start functino and lot of change in cookies

--- ario/response.py
+++ ario/response.py
@@ -3,14 +3,33 @@ from http.cookies import SimpleCookie
 
 class Response(Headers):
     def __init__(self, start_response):
-        __response_encoding = None
-        __cookies = SimpleCookie()
+        super().__init__()
+        self.__response_encoding = None
+        self.__cookies = SimpleCookie()
         self.start_response = start_response
+        self['Status'] = '200 OK'
 
 
     @property
     def response_encoding(self):
         return self.__response_encoding
+
+
+    @response_encoding.setter
+    def response_encoding(self, v):
+        self.__response_encoding = v
+
+
+    @property
+    def status(self):
+        return self['Status']
+
+
+    @status.setter
+    def status(self, v):
+        if v in None:
+            return
+        self['Status'] = v
 
 
     @response_encoding.setter
@@ -26,6 +45,16 @@ class Response(Headers):
         return None
 
 
+    def start(self):
+        cookie = self.__cookies.output()
+        if cookie:
+            for line in cookie.split('\r\n'):
+                print(line)
+                print(line.split(': '))
+                self.add_header(*line.split(': '))
+        self.start_response(self.status, self.items())
+
+
     @content_type.setter
     def content_type(self, v):
         if v is None:
@@ -34,11 +63,10 @@ class Response(Headers):
             self['Content-Type'] = '%s; charset=%s' % (v, self.response_encoding)
 
 
-
     def cookie(self, name, value, options=None):
         self.__cookies[name] = value
         if options:
-            for (k, v) in options:
+            for k, v in options.items():
                 self.__cookies[name][k] = v
 
 
@@ -48,3 +76,5 @@ class Response(Headers):
         if self.response_encoding:
             return buff.encode(self.__response_encoding)
         return buff
+
+
