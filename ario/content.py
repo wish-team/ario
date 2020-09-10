28fb827fc15ac40ae34ebe835ec99243b539cd2c
nothingrealm <shafaghi1378@gmail.com>
2020-09-10 11:17:35 +0430

    Added decorator for html

--- ario/content.py
+++ ario/content.py
@@ -1,6 +1,7 @@
 from functools import wraps
 import ujson
 
+
 def json(handler):
     @wraps(handler)
     def wrapper(request, response):
@@ -8,6 +9,17 @@ def json(handler):
         response.response_encoding = 'utf-8'
         body = handler(request, response)
         body = ujson.dumps(body)
+        body = response.encode_response(body)
+        return body
+    return wrapper
+
+
+def html(handler):
+    @wraps(handler)
+    def wrapper(request, response):
+        response.content_type = 'text/html'
+        response.response_encoding = 'utf-8'
+        body = handler(request, response)
         body = response.encode_response(body)
         return body
     return wrapper
