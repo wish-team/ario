d09b2db2a1fc1bcb7381721deeb778189d64f8cd
Shayan <shafaghi1378@gmail.com>
2020-09-11 20:42:23 +0430

    Importing jinja

--- ario/__init__.py
+++ ario/__init__.py
@@ -2,3 +2,4 @@ from .application import Application
 from .router import Endpoint, RouterController
 from .request import Request
 from .content import json, html
+from .jinja import jinja, setup_jinja
