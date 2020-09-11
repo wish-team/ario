ea1d575d6b8c30c19cba4242c7d23e8c91c880e6
Shayan <shafaghi1378@gmail.com>
2020-09-11 20:37:24 +0430

    Added *args to functions

--- ario/content.py
+++ ario/content.py
@@ -4,10 +4,10 @@ import ujson
 
 def json(handler):
     @wraps(handler)
-    def wrapper(request, response):
+    def wrapper(request, response, *args):
         response.content_type = 'application/json'
         response.response_encoding = 'utf-8'
-        body = handler(request, response)
+        body = handler(request, response, *args)
         body = ujson.dumps(body)
         body = response.encode_response(body)
         return body
@@ -16,10 +16,10 @@ def json(handler):
 
 def html(handler):
     @wraps(handler)
-    def wrapper(request, response):
+    def wrapper(request, response, *args):
         response.content_type = 'text/html'
         response.response_encoding = 'utf-8'
-        body = handler(request, response)
+        body = handler(request, response, *args)
         body = response.encode_response(body)
         return body
     return wrapper
