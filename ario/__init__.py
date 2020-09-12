5da9ccde4ca5a957df2fa0807d181bc3c943ccee
nothingrealm <shafaghi1378@gmail.com>
2020-09-12 11:31:58 +0430

    Importing redircet decorator

--- ario/__init__.py
+++ ario/__init__.py
@@ -1,6 +1,6 @@
 from .application import Application
 from .router import Endpoint, RouterController
 from .request import Request
-from .content import json, html
+from .content import json, html, redirect
 from .jinja import jinja, setup_jinja
 from .status import *
