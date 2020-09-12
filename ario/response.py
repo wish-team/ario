432711cf47a5139c63376ebc47cf9161865ea07f
nothingrealm <shafaghi1378@gmail.com>
2020-09-12 11:28:21 +0430

    Adding location property

--- ario/response.py
+++ ario/response.py
@@ -53,6 +53,39 @@ class Response(Headers):
             self['Content-Type'] = '%s; charset=%s' % (v, self.response_encoding)
 
 
+    @property
+    def content_type(self):
+        content_type = self.get('Content-Type')
+        if content_type:
+            return content_type.split(';')[0]
+        return None
+
+
+    @content_type.setter
+    def content_type(self, v):
+        if v is None:
+            del self['Content-Type']
+        else:
+            self['Content-Type'] = '%s; charset=%s' % (v, self.response_encoding)
+
+
+    @property
+    def location(self):
+        location = self.get('Location')
+        if location:
+            return location
+        return None
+
+
+    @location.setter
+    def location(self, v):
+        if v is None:
+            del self['Location']
+        else:
+            self['Location'] = v
+
+
+
     def cookie(self, name, value, options=None):
         self.__cookies[name] = value
         if options:
