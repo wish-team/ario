3bd192dec8619b59eabe96c3a0846535608ef8e7
amirhosein <amirhhh.shirani@yahoo.com>
2020-09-19 12:37:57 +0430

    requirements werkzeug

--- demo/run.py
+++ demo/run.py
@@ -2,6 +2,7 @@ import os
 import sys
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 from wsgiref.simple_server import make_server
+from werkzeug.serving import run_simple
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 PACKAGE_PARENT = '..'
@@ -23,27 +24,6 @@ class DashboardEndpoint(Endpoint):
         return params
 
 
-#
-# app = SharedDataMiddleware(Application(control), {
-#     '/static': os.path.join(os.path.dirname(__file__), 'templates/static')
-# })
-
-
-@control.route(method=['GET'], route="/static")
-def static_files():
-    print("STATIC_FILES")
-
-
-def create_app(with_static=True):
-    app = Application(control)
-    if with_static:
-        print("XXX")
-        app = SharedDataMiddleware(app, {
-            '/static': os.path.join(os.path.dirname(__file__), 'templates/static')
-        })
-    return app
-
-
 @control.default()
 @html
 def not_found(request, response):
@@ -60,9 +40,8 @@ def not_found(request, response):
 
 if __name__ == '__main__':
     app = Application(control)
-    run = make_server('', 5000, app)
+    app = SharedDataMiddleware(app, {
+        '/static': os.path.join(os.path.dirname(__file__), 'templates/static')
+    })
     print('Demo server started http://localhost:5000')
-    run.serve_forever()
-
-# ario = Ario()
-# ario.run()
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
