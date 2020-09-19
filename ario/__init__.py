ebe09760e6d9c18ab017705f786ddc8a798b748e
nothingrealm <shafaghi1378@gmail.com>
2020-09-19 13:00:24 +0430

    Removed a deleted package

--- ario/__init__.py
+++ ario/__init__.py
@@ -3,5 +3,4 @@ from .router import Endpoint, RouterController
 from .request import Request
 from .content import json, html, redirect
 from .jinja import jinja, setup_jinja
-from .werkzeug import Ario
 from .status import *
