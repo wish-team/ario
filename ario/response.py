fb51230e8b995df482590572fb7403808af710f8
nothingrealm <shafaghi1378@gmail.com>
2020-09-19 12:55:21 +0430

    Removed redundant code

--- ario/response.py
+++ ario/response.py
@@ -48,20 +48,6 @@ class Response(Headers):
             self['Content-Type'] = '%s; charset=%s' % (v, self.response_encoding)
 
     @property
-    def content_type(self):
-        content_type = self.get('Content-Type')
-        if content_type:
-            return content_type.split(';')[0]
-        return None
-
-    @content_type.setter
-    def content_type(self, v):
-        if v is None:
-            del self['Content-Type']
-        else:
-            self['Content-Type'] = '%s; charset=%s' % (v, self.response_encoding)
-
-    @property
     def location(self):
         location = self.get('Location')
         if location:
