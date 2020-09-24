728e73e9351e4113e06dadb41699d218df772a92
nothingrealm <shafaghi1378@gmail.com>
2020-09-24 12:32:52 +0330

    Don't render template if redirect header has been set

--- ario/jinja.py
+++ ario/jinja.py
@@ -14,15 +14,15 @@ def setup_jinja(path):
 def jinja(path):
     def render(func):
         template = __env.get_template(path)
-
         def handler(request, response, *args):
             response.content_type = 'text/html'
             response.response_encoding = 'utf-8'
             params = func(request, response, *args)
-            body = template.render(**params)
-            body = response.encode_response(body)
+            if not self.location:
+                body = template.render(**params)
+                body = response.encode_response(body)
+            else:
+                body = params
             return body
-
         return handler
-
     return render
